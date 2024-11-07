4ddbc98a41afb49fb86bf30f1046b2cb9c7e6f00
cristianomendieta <cristianomendieta@gmail.com>
2024-11-07 08:34:11 -0300

    feat: add files and results

--- baseline_train/binary.py
+++ baseline_train/binary.py
@@ -3,10 +3,23 @@ import os
 
 # Adicionar o diretório pai ao sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from utils import *
+from utils import prepare_data, train_models
 
 if __name__ == "__main__":
+    # Imprime o diretório de trabalho atual
     print(os.getcwd())
-    X, y = prepare_data("../tcc-results/data-results/data_prepared_enem_2022.parquet", "binary", "TP_ESCOLA")
-    train_models(X, y, ["baseline"], [53], "binary", "baseline_binary")
-    
+
+    # Define o caminho dos dados
+    data_path = os.path.join("..", "tcc-results", "data-results", "data_prepared_enem_2022.parquet")
+
+    # Prepara os dados
+    X, y = prepare_data(data_path, "binary", "TP_ESCOLA")
+
+    # Executa o treinamento utilizando o método baseline
+    train_models(
+        X, y,
+        reduction_methods=["baseline"],
+        n_components=[None],  # Nenhum componente necessário para o método baseline
+        average_type="binary",
+        experiment="baseline_experiment"
+    )
